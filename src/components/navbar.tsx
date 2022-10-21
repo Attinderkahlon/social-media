770ae3594309946c8e007c2df972bf80bcad6e41
Attinder <105198941+Attinderkahlon@users.noreply.github.com>
2022-10-22 09:58:19 +1100

    firebase setup

---
+++ src/components/navbar.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import { auth } from '../config/firebase'
+
+export const Navbar = () => {
+  return (
+    <div>
+      <Link to='/'>Home</Link>
+      <Link to='/login'>Login</Link>
+      <p>{auth.currentUser?.displayName}</p>
+      <img
+        src={auth.currentUser?.photoURL || ''}
+        width='100'
+        height='100'
+        alt='user '
+      />
+    </div>
+  )
+}
